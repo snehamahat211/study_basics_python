0a2e0618788ea2699eb9d84e4469d5a1210d71d4
snehamahat211 <snehamahat211@gmail.com>
2024-12-11 08:29:28 +0545

    whileloop

---
+++ loop.py/range.py
@@ -0,0 +1,14 @@
+# import random
+# num=[1,2,3,4,5,6,7,8,9,10,11]
+# for number in range(2):
+#   number=random.choice(num)
+#   print(number)
+
+# for number in range(1,11,3):
+#     print(number)
+
+total=0
+for number in range(1,101):
+    total+=number
+    print(number)
+print(total)
---
+++ loop.py/whileloop.py
@@ -0,0 +1,49 @@
+# n=int(input("enter a number"))
+# i=1
+# while i<=10:
+#    number=n*i
+#    print(f"{n}*{i}={number}")
+#    i+=1
+
+
+# list=[]
+# i=1
+# while i<=10:
+#    number=i*i
+#    print(number)
+#    list.append(number)
+#    i+=1
+
+
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# x=49
+# i=0
+# while i<len(nums):
+#    if (nums[i]==x):
+#     print("found at index",i)
+#    i+=1
+
+# break
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# x=49
+# i=0
+# while i<len(nums):
+#    if (nums[i]==x):
+#     print("found at index",i)
+#     break
+#    else:
+#      print("finding...")
+#    i+=1
+# print("end of loop")
+
+# continue
+i=0
+while i<=5:
+  if (i==3):
+    i+=1
+    continue #skip
+  print(i)
+  i+=1
+
+
+
