9bf830d08e64e968e0ac2c921d388b183830dc2e
snehamahat211 <snehamahat211@gmail.com>
2024-12-11 08:19:18 +0545

    length printing

---
+++ start.py
@@ -0,0 +1,22 @@
+# print ("beautiful"+input("what is your name?"))
+# input()will grt user input in console
+# print will print the word and the user input 
+
+# a=input("what is your name?")
+# print(len(a))
+
+
+# print(len(input("what is your name?")))
+
+name1="suni"
+print(name1)
+
+name="Sneha"
+print(name)
+
+length=len(name1)+len(name)
+print(length)
+
+
+
+
