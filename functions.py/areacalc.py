9276a44d03d8e575d4fc806fcda6accd531bd56c
snehamahat211 <snehamahat211@gmail.com>
2024-12-12 22:40:57 +0545

    practice

---
+++ functions.py/areacalc.py
@@ -0,0 +1,9 @@
+import math
+def area_calc(test_h,test_w):
+ number_of_cans=round((test_h*test_w)/coverage)
+ print(number_of_cans)
+test_h=int(input("height of wall:"))
+test_w=int(input("width of wall:"))
+coverage=5
+area_calc(test_h,test_w)
+print(f"the area is {area_calc}")
---
+++ functions.py/deffunc.py
@@ -0,0 +1,10 @@
+def functions():
+    print("SNEHA IS CUTIE")
+    print("she is beautiful")
+functions()
+
+# def my_function():
+    # Do this
+    # then do this
+    # finally do this
+# calling function
